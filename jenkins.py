d04a8271d84d64e5ac2da38c24af06356628baf0
Kevin Burke <kevin@twilio.com>
2013-02-08 01:31:06 -0800

    initial commit

---
+++ jenkins.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import argparse
+import ConfigParser
+import os
+import re
+import subprocess
+import sys
+import webbrowser
+
+from clint.textui import colored
+import requests
+
+def _get_remote_url(remote_name):
+    """ Retrieve the remote URL from a remote name like 'upstream' """
+    cmd = "git config --get remote.{}.url".format(remote_name)
+    try:
+        remote_url = subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        a = colored.red(
+            "Couldn't find a remote named {}\n".format(remote_name))
+        sys.stderr.write(str(a))
+        sys.exit(1)
+    return remote_url
+
+
+def _parse_git_remote_url(url):
+    """Parse Git remote URL's into a SRE_Match object
+
+    Matches user@<hostname>:<web_user>/<user_repo>
+    or      git://<hostname>/<web_user>/<user_repo>
+    """
+    ssh_regex = re.compile(r"""
+                           ^(
+                                (?P<git_user>\S+)@   # match user@ ssh url's
+                                |
+                                git://               # match git read-only url's
+                           )
+                           (?P<host>[^/:]+) # hostname
+                           [:/]             # separator
+                           (?P<user>[^/]+)
+                           /
+                           (?P<repo_name>.+?)
+                           \.git$
+                           """, re.X)
+    return ssh_regex.match(url)
+
+
+def _get_jenkins_host():
+    config = ConfigParser.ConfigParser({'port': '80'})
+    try:
+        config.readfp(open(os.path.expanduser("~") + '/.jenkinsrc'))
+    except IOError:
+        print colored.red("""Error: Config file .sshboxrc not found.
+
+Add your Jenkins URL configuration to a .jenkinsrc file like so:
+
+[jenkins]
+host = https://jenkins.mydomain.com
+port = 80""")
+        sys.exit(2)
+    host = config.get('jenkins', 'host')
+    port = config.get('jenkins', 'port')
+    return host, port
+
+
+def _get_git_branch():
+    # If no branch provided, default to the current branch
+    cmd = "git rev-parse --abbrev-ref HEAD"
+    try:
+        return subprocess.check_output(cmd, shell=True).strip('\n')
+    except subprocess.CalledProcessError:
+        sys.stderr.write(str(
+            colored.red("An error occurred trying to get the "
+                        "current git branch, command was {}".format(cmd))))
+        sys.exit(1)
+
+
+def _build_url(host, port, user, repo_name, branch):
+    port_part = "" if port == "80" else ":" + port
+    return host + port_part + "/job/{}.{}.{}".format(user, repo_name, branch)
+
+
+def _get_latest_build_url(api_response, default=None):
+    try:
+        return api_response['lastBuild']['url'] + 'console'
+    except KeyError:
+        return default
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Open a Jenkins job")
+    parser.add_argument('remote_name', type=str, default='origin', nargs='?',
+                        help="Name of the remote to open")
+    parser.add_argument('-b', '--branch', type=str, default=None, nargs='?',
+                        help="Git branch to open (defaults to current branch)")
+    args = parser.parse_args()
+
+
+    remote_url = _get_remote_url(args.remote_name)
+    match = _parse_git_remote_url(remote_url)
+    host, port = _get_jenkins_host()
+    branch = args.branch or _get_git_branch()
+    base_url = _build_url(host, port, match.group('user'), match.group('repo_name'),
+                     branch)
+
+    r = requests.get(base_url + '/api/json')
+    webbrowser.open_new_tab(_get_latest_build_url(r.json()))
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "jenkins",
+    version = "0.1",
+    description = "Open Jenkins URLs",
+    author = "Kevin Burke",
+    author_email = "kev@inburke.com",
+    packages = find_packages(),
+    py_modules = ['jenkins'],
+    entry_points = {
+        "console_scripts": [
+            "jenkins = jenkins:main",
+        ],
+    },
+    install_requires=[
+        'clint'
+    ]
+)
+
+
